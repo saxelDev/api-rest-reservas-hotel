41fc15bc825b6e58f4e7fd84bb0f174485159652
saxelDev <isaacjoelillatarco@gmail.com>
2025-07-14 12:26:48 -0400

    model factura

--- api_reservas/billing/models.py
+++ api_reservas/billing/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+from bookings.models import Reserva
+
+class Factura(models.Model):
+
+    factura_id = models.AutoField(primary_key=True)
+    reserva_id = models.ForeignKey(Reserva, on_delete=models.CASCADE)
+    fecha_emision = models.DateField(default=date.today)
+    subtotal = models.DecimalField(max_digits=6, decimal_places=2)
+    impuestos = models.DecimalField(max_digits=6, decimal_places=2)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    pagada = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'factura'
+        ordering = ["fecha_emision"]
+
