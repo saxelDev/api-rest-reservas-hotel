6643824099768a86e9c91a9a3e9ed41a40d94e6e
saxelDev <isaacjoelillatarco@gmail.com>
2025-07-14 12:18:23 -0400

    model evento

--- api_reservas/events/models.py
+++ api_reservas/events/models.py
@@ -1,1 +1,15 @@
+from django.db import models
 
+class Evento(models.Model):
+
+    id_evento = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, null=False)
+    descripcion = models.CharField(max_length=100)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    capacidad_maxima = models.PositiveIntegerField()
+    precio_entrada = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        db_table = "evento"
+        ordering = ["id_evento"]
